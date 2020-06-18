5894a9af113b1f3141d81af0c30518e46dd7706b
qixiangwe <m18745863572@163.com>
2020-06-18 14:42:58 +0800

    etcd server

--- library/EtcServer.go
+++ library/EtcServer.go
@@ -5,5 +5,37 @@ import (
 )
 
 type EtcServer struct {
-	clientv3.Client
+	etcClient *clientv3.Client
+	etcKv     clientv3.KV
+	etcLease  clientv3.Lease
+}
+
+var (
+	GEtcServer *EtcServer
+)
+
+func InitServer() (err error) {
+	var(
+		cli   *clientv3.Client
+		kv    clientv3.KV
+		lease clientv3.Lease
+	)
+
+	if cli, err = clientv3.New(clientv3.Config{
+		Endpoints:            nil,
+		DialTimeout:          0,
+	}); err != nil {
+		return
+	}
+
+	kv    = clientv3.NewKV(cli)
+	lease = clientv3.NewLease(cli)
+
+	GEtcServer = &EtcServer{
+		etcClient: cli,
+		etcKv:     kv,
+		etcLease:  lease,
+	}
+
+	return 
 }
