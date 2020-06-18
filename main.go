9253ee30e60bac4ac2dd14f5d7f1e870d9386746
qixiangwe <m18745863572@163.com>
2020-06-18 15:40:53 +0800

    update

--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
- 	"fmt"
-
+	"crontab/master"
+	"flag"
+	"runtime"
 )
 
+var (
+	confFile string
+)
+
+func initArgs() {
+	flag.StringVar(&confFile, "config", "config.json", "")
+	flag.Parse()
+}
+
+func initEnv() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
 func main()  {
-	fmt.Println("hello world")
+
+	var (
+		err error
+	)
+
+	initArgs()
+	initEnv()
+
+	if err = master.InitConfig(confFile); err != nil {
+		return
+	}
+
+	if err = master.InitApiServer(); err != nil {
+		return
+	}
 }
